efd86a58a1a8cd13777b1b9412389b329a5d9781
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2020-01-03 11:41:10 -0500

    Initial push to github

---
+++ src/main/java/com/lambdaschool/dadjokes/DadjokesApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.dadjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadjokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadjokesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lambdaschool/dadjokes/DadjokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.dadjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadjokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
